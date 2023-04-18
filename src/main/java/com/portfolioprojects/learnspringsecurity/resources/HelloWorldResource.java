a7df4000e39d5c1d9a12ab9092751a4d34ddf0d1
rdcruz0209 <robert.delacruz119@gmail.com>
2023-04-18 19:05:57 +0800

    Initial commit

---
+++ src/main/java/com/portfolioprojects/learnspringsecurity/resources/HelloWorldResource.java
@@ -0,0 +1,15 @@
+package com.portfolioprojects.learnspringsecurity.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldResource {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World v1";
+    }
+
+
+}
---
+++ src/main/java/com/portfolioprojects/learnspringsecurity/resources/TodoResource.java
@@ -0,0 +1,39 @@
+package com.portfolioprojects.learnspringsecurity.resources;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TodoResource {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    public static final List<Todo> TODOS_LIST = List.of(
+            new Todo("Robert", "Learn AWS"),
+            new Todo("Robert", "Learn Spring Boot"));
+
+    @GetMapping("/todos")
+    public List<Todo> retrieveAllTodos() {
+        return TODOS_LIST;
+    }
+
+
+    @GetMapping("/users/{username}/todos")
+    public Todo retrieveAllTodosForSpecificUser(@PathVariable String username, @RequestParam(defaultValue = "ABCDEF", required = false) String name) {
+        logger.info(name);
+        return TODOS_LIST.get(0);
+    }
+
+    @PostMapping("/users/{username}/todos")
+    public void createTodoForSpecificUser(@PathVariable String username, @RequestBody Todo todo) {
+        logger.info("Create {} for {}", todo, username);
+    }
+
+}
+
+record Todo(String username, String description) {
+
+}
